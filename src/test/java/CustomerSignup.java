9992ac40a2d3ac27bc0b462e7f80c09fc65ad73f
Emilio Martinez <emartinez@tacitknowledge.com>
2019-04-17 15:07:17 -0500

    added gUtils just in case they are needed

--- src/test/java/CustomerSignup.java
+++ src/test/java/CustomerSignup.java
@@ -4,42 +4,41 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import utils.driver.Driver;
+import utils.Gutils;
+import com.github.javafaker.Faker;
 
+
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 
 public class CustomerSignup {
 
-    @Step("Sign up as <customer> with email <customer@example.com> and <password>")
-    public void registerCustomerWith(String customer, String email, String password) {
+    Faker faker = new Faker(new Locale("en", "US"));
+
+    @Step("Sign up as <customer> with with fake email and <password> password")
+    public void registerCustomerWith(String customer, String password) {
         String locator = System.getenv("homepage_login_link");
         WebDriver webDriver = Driver.webDriver;
         webDriver.findElement(By.xpath(locator)).click();
 
         locator = System.getenv("homepage_modal_register_email");
-        webDriver.findElement(By.xpath(locator)).sendKeys(email);
+        webDriver.findElement(By.xpath(locator)).sendKeys(faker.internet().emailAddress());
 
         locator = System.getenv("homepage_modal_register_password");
-        webDriver.findElement(By.xpath(locator)).sendKeys(password + Keys.RETURN);
-        webDriver.manage().timeouts().implicitlyWait(1500, TimeUnit.MILLISECONDS);
-        locator = System.getenv("homepage_submit_registration_button");
-        webDriver.findElement(By.xpath(locator)).click();
+        webDriver.findElement(By.xpath(locator)).sendKeys(password);
+        webDriver.manage().timeouts().implicitlyWait(3500, TimeUnit.MILLISECONDS); //horrible
+        webDriver.findElement(By.xpath(locator)).sendKeys(Keys.RETURN);
+        /*locator = System.getenv("homepage_submit_registration_button");
+        webDriver.findElement(By.xpath(locator)).click(); --> not necessary at the moment */
 
-
-    }
-
-    @Step("Check if the user is logged in")
-    public void checkMyAccount() {
-        String locator = System.getenv("homepage_registered_myAccount");
-        WebDriver webDriver = Driver.webDriver;
-        webDriver.findElement(By.xpath(locator)).isDisplayed();
 
     }
 
     @Step("Verify that page title is <page>")
     public void checkPageTitle(String page) {
         String actualTitle;
-        actualTitle = Driver.webDriver.getTitle();
+        actualTitle = Gutils.getPageTitle();
         Assert.assertEquals(page, actualTitle);
 
     }
---
+++ src/test/java/utils/Gutils.java
@@ -0,0 +1,55 @@
+package utils;
+
+import org.junit.Assert;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utils.driver.Driver;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class Gutils
+    {
+        public static boolean isElementVisible(String xpath){
+            Boolean isVisible;
+            isVisible = Driver.webDriver.findElement(By.xpath(xpath)).isDisplayed();
+            return isVisible;
+        }
+
+       public static String getPageTitle(){
+
+          return Driver.webDriver.getTitle();
+       }
+
+        public static void waitForVisibility(String locator) {
+            WebDriverWait wait = new WebDriverWait(Driver.webDriver, 15);
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator)));
+        }
+
+        public static void selectValueFromDropDown(String value, String locator) {
+            waitForVisibility(locator);
+            Select dropdown = new Select(Driver.webDriver.findElement(By.xpath(locator)));
+            dropdown.selectByValue(value);
+        }
+
+        public static void switchToActiveWindow(){
+            Set<String> win = Driver.webDriver.getWindowHandles();
+            Iterator<String> it = win.iterator();
+            String name = "";
+            while (it.hasNext())
+                name = it.next();
+            Driver.webDriver.switchTo().window(name);
+        }
+
+        public static void placeOverAndClick(String locator) {
+            WebElement webElement = Driver.webDriver.findElement(By.xpath(locator));
+            Actions action = new Actions(Driver.webDriver);
+            action.moveToElement(webElement).click().perform();
+            action.click(webElement).perform();
+        }
+
+    }
