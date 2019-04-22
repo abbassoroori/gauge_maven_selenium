69e7414a284c67f4145bcc0be17d169c689b057b
Emilio Martinez <emartinez@tacitknowledge.com>
2019-04-21 23:29:14 -0500

    cleaned unused methods and renamed specs&concepts

--- specs/CustomerSignin.spec
+++ specs/CustomerSignin.spec
@@ -8,7 +8,7 @@ Customer Sign-in
 tags: sign-up, customer
 * Go to demo store
 * Check that store is up
-* Sign up as an existing customer
+* Sign in as an existing customer
 * Verify that Notes module is available
 
 
--- src/test/java/CustomerSignup.java
+++ src/test/java/CustomerSignup.java
@@ -1,27 +1,17 @@
 import com.thoughtworks.gauge.Step;
 import org.junit.Assert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import utils.driver.Driver;
 import utils.Gutils;
 import com.github.javafaker.Faker;
-
-
-
 import java.util.Locale;
-import java.util.concurrent.TimeUnit;
 
 
 public class CustomerSignup {
 
     Faker faker = new Faker(new Locale("en", "US"));
-
-    /*WebDriver webDriver = Driver.webDriver;
-
-    WebElement userfield =  webDriver.findElement(By.xpath(System.getenv("loginpage_user")));*/
-
 
 
     @Step("Sign in as <email> with password <password>")
@@ -42,25 +32,6 @@ public class CustomerSignup {
 
         webDriver.findElement(By.xpath(locator)).click();
 
-
-
-    }
-
-    @Step("Sign up as <customer> with with fake email and <password> password")
-    public void registerCustomerWith(String customer, String password) {
-        String locator = System.getenv("homepage_login_link");
-        WebDriver webDriver = Driver.webDriver;
-        webDriver.findElement(By.xpath(locator)).click();
-
-        locator = System.getenv("homepage_modal_register_email");
-        webDriver.findElement(By.xpath(locator)).sendKeys(faker.internet().emailAddress());
-
-        locator = System.getenv("homepage_modal_register_password");
-        webDriver.findElement(By.xpath(locator)).sendKeys(password);
-        webDriver.manage().timeouts().implicitlyWait(3500, TimeUnit.MILLISECONDS); //horrible
-        webDriver.findElement(By.xpath(locator)).sendKeys(Keys.RETURN);
-        /*locator = System.getenv("homepage_submit_registration_button");
-        webDriver.findElement(By.xpath(locator)).click(); --> not necessary at the moment */
 
 
     }
